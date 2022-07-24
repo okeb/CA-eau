49729755fadf4677a80ca6aed6a434d7e3c0b727
Oliver KEB <oliver.keb@icloud.com>
2022-07-24 11:00:01 +0200

    feature(eau): epreuve n°06 terminé

---
+++ eau06.rb
@@ -0,0 +1,51 @@
+# Créez un programme qui met en majuscule une lettre sur deux d’une chaîne de caractères.
+# Seule les lettres (A-Z, a-z) sont prises en compte.
+
+# LES FONCTIONS
+# -------------
+def capitalize_step char
+  abc_small = "abcdefghijklmnopqrstuvwxyz"
+  abc_big   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  not_found = true
+  counter = 0
+
+  while counter < 26 && not_found
+    if char == abc_small[counter]
+      char = abc_big[counter]
+      not_found = false
+    end
+    counter += 1
+  end
+  char
+end
+# -------------
+
+# LES ERREURS
+# -------------
+if ARGV.length != 1
+  puts 'erreur'
+  exit
+end
+# -------------
+
+# LES DATA
+# -------------
+char = ARGV[0].tr("\r\n", '!')
+# -------------
+
+# LA RESOLUTION
+# -------------
+result = []
+(0..char.length).each do |l|
+  if l.even?
+    result.append(capitalize_step char[l])
+  else
+    result.append char[l]
+  end
+end
+# -------------
+
+# RESULTAT(S)
+# -------------
+puts result.join ''
+# -------------
