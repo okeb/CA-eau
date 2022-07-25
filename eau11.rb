50dd6d6ee1afa0ef4c7ba4be0cd193df2bd69023
Oliver KEB <oliver.keb@icloud.com>
2022-07-25 09:21:09 +0200

    feature(eau): epreuve n°11 terminé

---
+++ eau11.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+# Créez un programme qui affiche la différence minimum absolue entre deux éléments d’un tableau
+# constitué uniquement de nombres. Nombres négatifs acceptés.
+
+
+# LES FONCTIONS
+# -------------
+def is_numeric(char)
+  !char.match(/\A[+-]?\d+?(_?\d+)*(\.\d+e?\d*)?\Z/).nil?
+end
+
+def return_min_diff_abs(list)
+  idx_a = idx_b = x = 0
+  mid_diff_abs = nil
+  last_idx = list.index(list[-1])
+  while idx_a <= last_idx
+    idx_b = idx_a + 1
+    while idx_b <= last_idx
+      diff_abs = list[idx_a].to_i - list[idx_b].to_i
+      diff_abs *= -1 if diff_abs.negative?
+      mid_diff_abs = diff_abs if mid_diff_abs.nil? || mid_diff_abs > diff_abs
+      idx_b += 1
+    end
+    idx_a += 1
+  end
+  mid_diff_abs
+end
+# -------------
+
+# LES ERREURS
+# -------------
+if ARGV.length >= 2
+  (0..(ARGV.index(ARGV[-1]))).each do |i|
+    unless is_numeric(ARGV[i])
+      puts 'error'
+      exit
+    end
+  end
+else
+  puts 'error'
+  exit
+end
+# -------------
+
+# LES DATA
+# -------------
+list = ARGV
+# -------------
+
+# LA RESOLUTION
+# -------------
+result = return_min_diff_abs(list)
+# -------------
+
+# RESULTAT(S)
+# -------------
+puts result
+# -------------
